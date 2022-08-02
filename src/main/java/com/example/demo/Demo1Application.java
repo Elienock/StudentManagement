de50a1f07708ac91db47cfaea6ce265bc9b226c2
Elienock <lubayaelienock@gmail.com>
2022-08-02 08:09:31 +0200

    created the student class

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
 
+    @GetMapping
+    public List<Student> hello(){
+        return List.of(
+                new Student(
+                            1L,"Tresor", "tresor@gmail,com", LocalDate.of(2001, Month.APRIL,5),21
+                )
+        );
+    }
 }

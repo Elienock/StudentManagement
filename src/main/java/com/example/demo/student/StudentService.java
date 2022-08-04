cee4c4481e2daf5041bb942ca4dfb3aa8d2b9ba9
Elienock <lubayaelienock@gmail.com>
2022-08-04 14:15:06 +0200

    added the entity

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.demo.student;
+
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,"Tresor", "tresor@gmail,com", LocalDate.of(2001, Month.APRIL,5),21
+                )
+        );
+    }
+}
